3ac5a0f814c6ba727bc948fb4df6ec119684e75e
Rachel <racmocon@gmail.com>
2019-10-07 11:55:34 -0400

    Removed sass-related dependencies and reverted js implimentation to original structure.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'var',
     library: 'Component'
   },
